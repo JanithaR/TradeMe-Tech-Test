656d3da8a5e7685e6c165ba633754e57c0d8b61c
JANITHA-DEKSTOP\Janitha <janitha.ranawansa@gmail.com>
2019-01-21 22:20:17 +0530

    Removed redundant colors

--- TradeMeTechTest/app/res/colors.js
+++ TradeMeTechTest/app/res/colors.js
@@ -1,7 +1,5 @@
 export default {
     tradeMeBlue: '#348bcb',
     tradeMeYellow: '#f8ae2b',
-    accordionBackgroundColor: '#ECEFF1',
-    accordionBorderBottomColor: '#CFD8DC',
     yellowHeaderTextColor: '#ad5319'
 };
